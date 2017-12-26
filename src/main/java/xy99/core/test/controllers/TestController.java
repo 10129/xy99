8767cf9a99573f7ad3cf942e391f2a635ee36dd7
10129 <1019533934@qq.com>
2017-12-26 21:01:09 +0800

    初始化依赖引入

---
+++ src/main/java/xy99/core/test/controllers/TestController.java
@@ -0,0 +1,24 @@
+package xy99.core.test.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by xieshuai on 2017/12/26.
+ */
+@Controller
+@RequestMapping("/test")
+public class TestController {
+
+    @RequestMapping(value="/query")
+    public void test(HttpServletRequest request, @RequestParam String id){
+
+        String AAA=id;
+    }
+
+
+}
